696b7e0291d8cf68731c478255117f2b6841d0d7
linwei <linwei7310@163.com>
2021-08-15 17:38:55 +0800

    工作初建

---
+++ provider-payment/src/main/java/cn/lw/entity/Payment.java
@@ -0,0 +1,18 @@
+package cn.lw.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: linwei
+ * @CreteTime: 2021/8/15
+ * @Description:TODO
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    private long id;
+    private String name;
+}
---
+++ provider-payment/src/main/java/cn/lw/run/Provider_Payment.java
@@ -0,0 +1,13 @@
+package cn.lw.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Provider_Payment{
+
+    public static void main(String[] args) {
+        SpringApplication.run(Provider_Payment.class, args);
+    }
+
+}
