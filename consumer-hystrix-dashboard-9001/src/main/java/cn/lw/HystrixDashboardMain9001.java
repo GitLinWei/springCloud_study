37346360675dd8cd4e1ddf2f2c072f91f0c75073
linwei <linwei7310@163.com>
2021-09-27 16:12:38 +0800

    hystrix-dashboard图形界面监控

---
+++ consumer-hystrix-dashboard-9001/src/main/java/cn/lw/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package cn.lw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Author: linwei
+ * @CreteTime: 2021/9/27 15:58
+ * @Description:TODO
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
