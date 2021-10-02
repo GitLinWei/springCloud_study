3905a3a4b5c13023f8afe2be0c9993668c2dc777
linwei <linwei7310@163.com>
2021-10-02 14:00:55 +0800

    geteway 路由、断言、过滤器

---
+++ geteway-9527/src/main/java/cn/lw/filter/GeteWayFilter.java
@@ -0,0 +1,35 @@
+package cn.lw.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author linwei
+ * @Date 2021/10/2 13:48
+ * @Description TODO
+ */
+@Component
+@Slf4j
+//自定义过滤器必须实现GlobalFilter、Ordered这两个接口
+public class GeteWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("****进入过滤器******");
+        String uname=exchange.getRequest().getQueryParams().getFirst("uname");
+        if(uname==null){
+            log.info("****uname为null*****");
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ geteway-9527/src/test/java/T1.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @Author linwei
+ * @Date 2021/10/2 10:46
+ * @Description TODO
+ */
+public class T1 {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime=ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
