20e80a8ece973cafbb667eed9b65adfd3fe93158
linwei <linwei7310@163.com>
2021-10-02 14:50:24 +0800

    geteway 路由、断言、过滤器

--- geteway-9527/src/main/java/cn/lw/filter/GeteWayFilter.java
+++ geteway-9527/src/main/java/cn/lw/filter/GeteWayFilter.java
@@ -13,23 +13,23 @@ import reactor.core.publisher.Mono;
  * @Date 2021/10/2 13:48
  * @Description TODO
  */
-@Component
-@Slf4j
-//自定义过滤器必须实现GlobalFilter、Ordered这两个接口
-public class GeteWayFilter implements GlobalFilter, Ordered {
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("****进入过滤器******");
-        String uname=exchange.getRequest().getQueryParams().getFirst("uname");
-        if(uname==null){
-            log.info("****uname为null*****");
-            return exchange.getResponse().setComplete();
-        }
-        return chain.filter(exchange);
-    }
-
-    @Override
-    public int getOrder() {
-        return 0;
-    }
-}
+//@Component
+//@Slf4j
+////自定义过滤器必须实现GlobalFilter、Ordered这两个接口
+//public class GeteWayFilter implements GlobalFilter, Ordered {
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        log.info("****进入过滤器******");
+//        String uname=exchange.getRequest().getQueryParams().getFirst("uname");
+//        if(uname==null){
+//            log.info("****uname为null*****");
+//            return exchange.getResponse().setComplete();
+//        }
+//        return chain.filter(exchange);
+//    }
+//
+//    @Override
+//    public int getOrder() {
+//        return 0;
+//    }
+//}
