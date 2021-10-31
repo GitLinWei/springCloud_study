435f65b80c772ed433e41ba6fd40c91bde1b328f
linwei <linwei7310@163.com>
2021-10-31 21:36:27 +0800

    stream 分组、持久化

---
+++ stream-rabbitmq-consumer8803/src/main/java/cn/lw/StreamMQMain8803.java
@@ -0,0 +1,16 @@
+package cn.lw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author linwei
+ * @Date 2021/10/31 18:11
+ * @Description TODO
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
